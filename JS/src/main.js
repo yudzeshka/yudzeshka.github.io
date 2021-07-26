56480d77ede0aa7e24de5e4b861e54d67c032b05
Maksim Yudzeshka <yudzeshka@gmail.com>
2021-07-26 12:51:31 +0300

    Triangles_app_v1.2

--- JS/src/main.js
+++ JS/src/main.js
@@ -7,7 +7,7 @@ import { getAnswerPhrase } from "./triangles/output.js";
 const sides = [
   { sideName: "a", sideLength: 300 },
   { sideName: "b", sideLength: 400 },
-  { sideName: "c", sideLength: 1100 },
+  { sideName: "c", sideLength: 800 },
 ];
 
 const lowerBound = 0;
@@ -20,7 +20,7 @@ function renderErrorScreen(messages) {
   const errorListElement = document.createElement("div");
   const sadImg = document.createElement("img");
 sadImg.src = "./media/img/sadsmile.png";
-sadImg.className = "sadImg";
+sadImg.className = "img";
 
   messages.forEach((message) => {
     const messageItem = document.createElement("p");
@@ -29,18 +29,30 @@ sadImg.className = "sadImg";
     errorListElement.append(messageItem);
     errorListElement.append(sadImg);
   });
-
   fragment.append(errorListElement);
-
   return fragment;
 }
 
 function renderAnswerScreen(answerPhrase, answer) {
+  const answerBlock = document.createElement("div");
   const answerElement = document.createElement("p");
   answerElement.innerHTML = answerPhrase;
+  const smileImg = document.createElement("img");
+  smileImg.src = "./media/img/funnysmile.png";
+smileImg.className = "img";
+const cryImg = document.createElement("img");
+  cryImg.src = "./media/img/crysmile.png";
+cryImg.className = "img";
   answerElement.className = answer ? "positive" : "negative";
+  answerBlock.prepend(answerElement);
+  
+  if (answer) {answerBlock.append(smileImg)} else {
+    answerBlock.append(cryImg);
+  }
+    
+  
 
-  return answerElement;
+  return answerBlock;
 }
 
 function render(appRoot) {
@@ -68,6 +80,7 @@ function main() {
   dano.innerHTML = `Даны стороны: "${sides[0].sideName}" длиной  ${sides[0].sideLength}, "${sides[1].sideName}" длиной  ${sides[1].sideLength}, "${sides[2].sideName}" длиной  ${sides[2].sideLength}.`;
   data.append(dano);
   appDiv.before(data);
+
   const diap = dano.cloneNode(true);
   diap.className = "diapstyles";
   diap.innerHTML = `Значения сторон необходимо вводить в диапозоне от ${lowerBound} до ${upperBound}.`;
